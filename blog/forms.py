ee62412d05de407d99c750dabf73457c6076538b
Tomasz <tete10@wp.pl>
2017-01-25 15:03:12 +0100

    add comment adding

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Post
+from .models import Post, Comment
 
 class PostForm(forms.ModelForm):
 
@@ -8,6 +8,17 @@ class PostForm(forms.ModelForm):
         fields = ('title', 'text',)
         widgets = {
             'title' : forms.TextInput(attrs={'class': 'w3-input'}),
+            'text' : forms.Textarea(attrs={'cols' : '100%','class' : 'w3-input'}),
+
+        }
+
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = Comment
+        fields = ('author', 'text',)
+        widgets = {
+            'author' : forms.TextInput(attrs={'class': 'w3-input'}),
             'text' : forms.Textarea(attrs={'cols' : '100%','class' : 'w3-input'}),
 
         }
